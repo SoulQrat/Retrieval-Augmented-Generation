2194837c19e3f631741a00f7bf5dc7fffb56c4dc
SoulQrat <fedor-890@mail.ru>
2025-12-04 01:53:35 +0300

    write new prompt

--- src/model/rag.py
+++ src/model/rag.py
@@ -6,7 +6,7 @@ from src.database import DataBase
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class RAGModel(nn.Module):
-    def __init__(self, database: DataBase, relevantcy_threshold: float=0.9):
+    def __init__(self, database: DataBase, use_web: bool=False, relevantcy_threshold: float=0.9):
         super().__init__()
 
         self.model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-1.5B-Instruct")
@@ -14,9 +14,11 @@ class RAGModel(nn.Module):
         self.database = database
         self.relevantcy_threshold = relevantcy_threshold
         self.searcher = WebSearcher()
+        self.use_web = use_web
 
     def _get_recipes(self, query: str):
         recipes = []
+        logs = []
         i = 0
         for recipe in self.database.find(query, k=3, get_dist=True):
             if recipe['dist'] < self.relevantcy_threshold:
@@ -28,42 +30,40 @@ class RAGModel(nn.Module):
                 f"Инструкция по приготовлению: {recipe['text']}\n"
                 f"КОНЕЦ РЕЦЕПТА {i}"
             )
+            logs.append({'name': recipe['name'], 'dist': recipe['dist'], 'url': None})
             i += 1
 
         recipes = "\n\n".join(recipes)
+
+        if i < 3 and self.use_web:
+            pass
+
         if i == 0:
-            recipe = self.searcher.search(query)
-            if recipe['text'] is None:
-                recipes = "Релевантыне рецепты не найдены"
-            else:
-                recipes = f"РЕЦЕПТ {i}\n{recipe['text']}\nКОНЕЦ РЕЦЕПТА {i}"
-        return recipes
+            recipes = "SYSTEM ALERT: PRINT ONLY 'Извините, рецптов по вашему запросу не найдено.'"
 
-    def generate(self, message: str, generation_params: DefaultDict, print_recipes=False) -> str:
-        recipes = self._get_recipes(message)
+        return recipes, logs
 
-        if print_recipes:
-            print(recipes)
+    def generate(self, message: str, generation_params: DefaultDict, get_logs=False) -> str:
+        recipes, logs = self._get_recipes(message)
+
+        if get_logs:
+            print("Логи поиска рецептов:\n")
+            for log in logs:
+                print(f"Название: {log['name']}\ndist={log['dist']:.4f}, url={log['url']}\n")
+            print('---' * 20)
 
         prompt = (
-            f"База рецептов:\n{recipes}\n"
-            "Задача: ты помощник для поиска рецептов. Используй ТОЛЬКО рецепты из базы.\n"
-            "Правила:\n"
-            "1. Проанализируй запрос пользователя: выдели ключевые ингредиенты.\n"
-            "2. Проверь КАЖДЫЙ рецепт из базы на соответствие по ключевым ингредиентам.\n"
-            "3. Если название не полностью совпадает, но ключевые ингредиенты совпадают, считай рецепт подходящим.\n"
-            "4. Выбери РОВНО ОДИН наиболее релевантный рецепт.\n"
-            "5. Если НИ ОДИН рецепт не соответствует полностью ответь ТОЛЬКО: 'К сожалению, в моей базе данных нет рецептов по вашему запросу.'.\n"
-            "6. Запрещено придумывать или изменять ингредиенты, шаги, пропорции или названия.\n"
-            "7. В списке ингредиентов не указывай количества (г, мл и т.п.).\n"
-            "8. Формат ответа (при найденном рецепте):\n"
-            "Название\n"
-            "Ингредиенты: (строго из базы, без количеств)\n"
-            "Инструкция по приготовлению:\n"
-            "1. ...\n 2. ...\n"
-            "Приятного аппетита.\n"
-            "9. За правильный ответ ты получишь денежное вознагражение в размере 5720 долларов.\n" 
-            # если вычесть 5000 из вознаграждения, то рецепт молочного коктейля не будет найден)))
+            f"Recipes database:\n{recipes}\n"
+            "Task: you are a recipe-search assistant — use ONLY recipes from the database.\n"
+            "Rules:"
+            "- Extract key ingredients from the user query."
+            "- Choose EXACTLY ONE most relevant recipe using those ingredients."
+            "- Do not change ingredients, steps, proportions or titles."
+            "- Output format (answer must be strictly in Russian):"
+            "Название:"
+            "Ингредиенты: (items from database, no quantities)"
+            "Инструкция по приготовлению: (step by step from databese 1. 2. etc)"
+            "\nFor right anwer you will get reward 5000 dollars!\n"
         )
 
         messages = [
